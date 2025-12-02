580d7954f858f9f071ad8a8b34537a35a948455d
Mahyar Mousavinia <mahyar.mousavi.1408@gmail.com>
2025-12-02 17:40:07 +0400

    forgot to add these two file :D

---
+++ src/js/View/resultsView.js
@@ -0,0 +1,46 @@
+'use strict';
+import icons from 'url:../../img/icons.svg';
+import View from './View.js';
+
+class ResultsView extends View {
+    _parentElement = document.querySelector('.results');
+    _searchElement = document.querySelector('.search');
+
+    addHandlerSearchResult(handlerFn) {
+        this._searchElement.addEventListener('submit', function (e) {
+            e.preventDefault();
+            handlerFn();
+        });
+    }
+
+    _getHTML() {
+        if (!this._data) return;
+        this._parentElement.innerHTML = '';
+
+        const html = this._data
+            .map(obj => {
+                return `
+          <li class="preview">
+            <a class="preview__link preview__link--active" href="#${obj.id}">
+              <figure class="preview__fig">
+                <img src="${obj.image}" alt="Test" />
+              </figure>
+              <div class="preview__data">
+                <h4 class="preview__title">${obj.title}</h4>
+                <p class="preview__publisher">${obj.publisher}</p>
+                <div class="preview__user-generated">
+                  <svg>
+                    <use href="${icons}#icon-user"></use>
+                  </svg>
+                </div>
+              </div>
+            </a>
+          </li>
+    `;
+            })
+            .join('');
+
+        return html;
+    }
+}
+export default new ResultsView();
